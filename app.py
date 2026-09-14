1eab68ce3e24b0cb557de641c898678f1c04bcf8
a99718280-commits <a99718280@gmail.com>
2026-09-14 22:10:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -252,6 +252,7 @@ def send_push(item):
                 messaging.send(msg)
                 sent += 1
             except Exception as e:
+                            print("FCM SEND ERROR:", repr(e), flush=True)
                 if 'registration-token-not-registered' in str(e).lower() or 'not found' in str(e).lower():
                     invalid.append(token)
         if invalid:
