13c622d36dc92aa2a26d1bd6c169b09b8c5994bc
a99718280-commits <a99718280@gmail.com>
2026-09-15 17:21:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ SOURCES = [
     },
     {
         'chain': '메가박스',
-       'url': 'https://www.megabox.co.kr/event',
+      'url': 'https://m.megabox.co.kr/event/movie',
         'booking_url': 'https://www.megabox.co.kr/booking',
     },
 ]
